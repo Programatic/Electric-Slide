3fbe2992fa69bd11a3163bc2818910d2f91cb1df
Ford Smith <ford.smith@case.edu>
2022-01-30 17:23:43 -0500

    WORKING WHOOOO

---
+++ slide.py
@@ -0,0 +1,54 @@
+import argparse
+import requests
+from rpi_ws281x import PixelStrip, Color
+from threading import Thread
+
+# LED strip configuration:
+LED_COUNT = 300        # Number of LED pixels.
+LED_PIN = 18          # GPIO pin connected to the pixels (18 uses PWM!).
+# LED_PIN = 10        # GPIO pin connected to the pixels (10 uses SPI /dev/spidev0.0).
+LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA = 10          # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 20  # Set to 0 for darkest and 255 for brightest
+LED_INVERT = False    # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
+tempo = 1000
+t = 30. / tempo;
+mult = tempo/60.;
+
+color = Color(255, 128, 0)
+
+def update():
+    global tempo
+    global t
+    global mult
+    global color
+    while True:
+        try:
+            r = requests.get('http://192.168.1.16:8888/tempo')
+            parts = r.text.split()
+            tempo = float(parts[0])
+            r = int(parts[1])
+            g = int(parts[2])
+            b = int(parts[3])
+
+            color = Color(r, g, b)
+
+            if tempo != 0:
+                t = 30. / tempo;
+            mult = tempo/60.;
+
+            print(tempo)
+        except Error:
+            pass
+
+
+def setStripColor(strip, color):
+    for i in range(strip.numPixels()):
+        strip.setPixelColor(i, color)
+
+# Main program logic follows:
+if __name__ == '__main__':
+    thread1 = Thread( target=update )
+    thread1.start()
--- test.js
+++ test.js
@@ -1,54 +1,97 @@
 const express = require('express');
-const querystring = require('querystring');
+const qs = require('querystring');
 const axios = require('axios');
 
 const port = 8888;
-const ACCESS_TOKEN = "BQCy9jU7Q1xcprG1NIQau1fHnPxtCsVI5MIs7FUduxi9Ftk5zGVGMDukSoaNAiMHpYj1384KX0RXvs9tuD0254PoCktkcft1ci--wzhWi9H_2Vsgsu_RkazAChTjHxsxRnUyyhhGzF6b8vHCmgO5kWaBQQ";
+//var ACCESS_TOKEN = "BQDk-b8r8FZvjjNxUEtOyws5O3FzMVORMg1GVPfEIOLZzbfvwvhCLo8l2WWWfYZ96Hdn7LZSMJq3ldtmzJI_e9hlNO5tF2rCP_yJOmFJ-aoZH0b-mvDHMk8bG9jyppyj-2Mu0Aa1hkEwOBXXjgTfW4s4_Q";
+ACCESS_TOKEN = '';
 
 var client_id = '850d1647e0524185934a75a886c5bc1e';
-var redirect_uri = 'http://localhost:8888/callback';
+var redirect_uri = 'http://192.168.1.16:8888/callback';
 
 var app = express();
 
 var tempo = 0;
+var color = [0, 0, 0];
 
-app.get('/login', function(req, res) {
+const CHROMA_COLOR = [
+    [229, 76, 78],
+    [223, 132, 74],
+    [228, 171, 81],
+    [227, 199, 73],
+    [223, 228, 78],
+    [174, 215, 71],
+    [63, 188, 70],
+    [63, 169, 180],
+    [64, 124, 180],
+    [78, 69, 179],
+    [141, 69, 183],
+    [202, 69, 147]
+];
 
-  var state = "abcdefghijklmnop" 
-  var scope = 'user-read-playback-state';
+app.get('/login', function (req, res) {
 
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: client_id,
-      scope: scope,
-      redirect_uri: redirect_uri,
-      state: state
-    }));
+    var state = "abcdefghijklmnop"
+    var scope = 'user-read-playback-state';
+
+    res.redirect('https://accounts.spotify.com/authorize?' +
+        qs.stringify({
+            response_type: 'code',
+            client_id: client_id,
+            scope: scope,
+            redirect_uri: redirect_uri,
+            state: state
+        }));
 });
 
 app.get('/callback', (req, res) => {
-    console.log(req);
+    var data = qs.stringify({
+        'grant_type': 'authorization_code',
+        'code': req.query["code"],
+        'redirect_uri': redirect_uri
+    });
+    var config = {
+        method: 'post',
+        url: 'https://accounts.spotify.com/api/token',
+        headers: {
+            'Authorization': 'Basic ODUwZDE2NDdlMDUyNDE4NTkzNGE3NWE4ODZjNWJjMWU6OTQ4ODQxOTRlYWM2NDc0ZmE1NWU3MWNiNWI5NTJhMDU=',
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        data: data
+    };
+
+    axios(config)
+        .then(function (response) {
+            let data = response.data;
+            ACCESS_TOKEN = data["access_token"];
+            console.log(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+
+    res.status(200);
 });
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Example app listening on port ${port}`)
 });
 
 app.get('/tempo', (req, res) => {
-    res.send(tempo.toString());
+    res.send(tempo + " " + Math.round(color[0]) + " " + Math.round(color[1]) + " " + Math.round(color[2])).status(200);
+    res.end();
 });
 
 async function audio_analysis(id) {
     var config = {
-      method: 'get',
-      url: 'https://api.spotify.com/v1/audio-analysis/' + id,
-      headers: { 
-        'Content-Type': 'application/json', 
-        'Authorization': 'Bearer ' + ACCESS_TOKEN,
-      }
+        method: 'get',
+        url: 'https://api.spotify.com/v1/audio-analysis/' + id,
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + ACCESS_TOKEN,
+        }
     };
-    
+
     let rsp = await axios(config);
     return rsp.data;
 }
@@ -58,30 +101,88 @@ async function audio_analysis(id) {
 function current_tempo(song_obj, time_since_start) {
     let sections = song_obj['sections'];
     for (let i = sections.length - 1; i >= 0; i--) {
-        if (time_since_start/1000 >= sections[i]['start']) {
+        if (time_since_start / 1000 >= sections[i]['start']) {
             return sections[i]['tempo'];
         }
     }
 }
 
-var config = {
-  method: 'get',
-  url: 'https://api.spotify.com/v1/me/player',
-  headers: { 
-    'Content-Type': 'application/json', 
-    'Authorization': 'Bearer ' + ACCESS_TOKEN,
-  }
-};
+function current_pitches(song_obj, time_since_start) {
+    let ret = [0, 0, 0];
+    let sections = song_obj['segments'];
+    for (let i = sections.length - 1; i >= 0; i--) {
+        if (time_since_start / 1000 >= sections[i]['start']) {
+            let pitches = sections[i]['pitches'];
+            for (let j = 0; j < pitches.length; j++) {
+                let scale = pitches[j];
 
-axios(config)
-.then(async function (response) {
-    let song_id = response.data['item']['id'];
-    let progress_ms = response.data['progress_ms'];
+                if (scale === 1)
+                    return CHROMA_COLOR[j];
 
-    let song_obj = await audio_analysis(song_id);
-    tempo = current_tempo(song_obj, progress_ms);
-    console.log('Set Tempo: ' + tempo);
-})
-.catch(function (error) {
-  console.log(error);
-});
+                ret[0] += scale * CHROMA_COLOR[j][0];
+                ret[1] += scale * CHROMA_COLOR[j][1];
+                ret[2] += scale * CHROMA_COLOR[j][2];
+
+            }
+
+            ret[0] /= pitches.length;
+            ret[1] /= pitches.length;
+            ret[2] /= pitches.length;
+
+            return ret;
+        }
+    }
+}
+
+let song_obj = {};
+let last_update = 0;
+let progress_ms = 0;
+let updated = false;
+
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms))
+
+async function get_song() {
+    var config = {
+        method: 'get',
+        url: 'https://api.spotify.com/v1/me/player',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + ACCESS_TOKEN,
+        }
+    };
+
+    axios(config).then(async (response) => {
+
+        if (response.data === '') {
+            await delay(5000);
+            return;
+        }
+
+        if (!response.data['item'].hasOwnProperty('id'))
+            return;
+
+        let song_id = response.data['item']['id'];
+        progress_ms = response.data['progress_ms'];
+        song_obj = await audio_analysis(song_id);
+        last_update = Date.now();
+
+        tempo = current_tempo(song_obj, progress_ms);
+        console.log('Set Tempo: ' + tempo);
+
+        updated = true;
+    }).catch((e) => {
+        //console.log(e);
+        console.log("Error with api");
+    });
+
+}
+
+function update_color() {
+    if (!updated)
+        return;
+
+    color = current_pitches(song_obj, progress_ms + Date.now() - last_update);
+}
+
+setInterval(get_song, 500);
+setInterval(update_color, 50);
